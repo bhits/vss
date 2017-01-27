6e5b58b79de8bded32da5ee6f49e1539a59a427d
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-26 21:11:13 -0500

    Create initial project for vss
    #12553 - As a developer, will have created a value set API that is componentized and integrates with C2S architecture

---
+++ vss/src/main/java/gov/samhsa/c2s/ValueSetServiceApiApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValueSetServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValueSetServiceApiApplication.class, args);
+	}
+}
