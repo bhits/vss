8a148595cf5839b42e9ebaa163ad13e458a6a151
JiahaoLi <Jiahao.Li@feisystems.com>
2017-01-27 11:58:45 -0500

    Enable security for accessing management endpoint
    #12553 - As a developer, will have created a value set API that is componentized and integrates with C2S architecture

--- vss/src/main/java/gov/samhsa/c2s/ValueSetServiceApiApplication.java
+++ vss/src/main/java/gov/samhsa/c2s/ValueSetServiceApiApplication.java
@@ -2,11 +2,17 @@ package gov.samhsa.c2s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
+@EnableDiscoveryClient
+@EnableFeignClients
 public class ValueSetServiceApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ValueSetServiceApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ValueSetServiceApiApplication.class, args);
+    }
 }
