e1e57c23d118d37764642d35f2e96426ac10cf21
sadhch <sadhana.chandravanka@feisystems.com>
2017-05-16 18:04:07 -0400

    Integrate with spring data rest
    #13775 - As a developer, I will have implemented the VSS Backend [v3.0]

--- vss/src/main/java/gov/samhsa/c2s/vss/domain/CodeSystemVersionRepository.java
+++ vss/src/main/java/gov/samhsa/c2s/vss/domain/CodeSystemVersionRepository.java
@@ -1,9 +1,11 @@
 package gov.samhsa.c2s.vss.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface CodeSystemVersionRepository extends JpaRepository<CodeSystemVersion, Long> {
     Optional<CodeSystemVersion> findTopByCodeSystemCodeSystemOidOrderByVersionOrderDesc(String codeSystemOid);
 }
--- vss/src/main/java/gov/samhsa/c2s/vss/domain/ValueSetCategoryRepository.java
+++ vss/src/main/java/gov/samhsa/c2s/vss/domain/ValueSetCategoryRepository.java
@@ -1,6 +1,8 @@
 package gov.samhsa.c2s.vss.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ValueSetCategoryRepository extends JpaRepository<ValueSetCategory, Long> {
 }
