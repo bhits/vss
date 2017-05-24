5f7b595033f0ba3c776912302f9052b2a998d9bc
sadhch <sadhana.chandravanka@feisystems.com>
2017-05-24 14:14:30 -0400

    Controller configuration for constraint violation exception
    #13775 - As a developer, I will have implemented the VSS Backend [v3.0]

---
+++ vss/src/main/java/gov/samhsa/c2s/vss/config/ControllerConfig.java
@@ -0,0 +1,20 @@
+package gov.samhsa.c2s.vss.config;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+
+@ControllerAdvice
+public class ControllerConfig {
+
+    // This will set response status to 400 bad request instead of defaulting to 500 internal server error
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST, reason = "Invalid Data sent to server")
+    public void handleConstraintValidation() {
+        // TODO :: Need to give more details on exact violation and the model inforamtion
+    }
+
+}
