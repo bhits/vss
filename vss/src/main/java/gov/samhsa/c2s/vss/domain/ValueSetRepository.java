cc949732d32f41ef038999850a928366853c3857
sadhch <sadhana.chandravanka@feisystems.com>
2017-05-30 09:57:15 -0400

    Add new find method that used in upload feature
    #13775 - As a developer, I will have implemented the VSS Backend [v3.0]

--- vss/src/main/java/gov/samhsa/c2s/vss/domain/ValueSetRepository.java
+++ vss/src/main/java/gov/samhsa/c2s/vss/domain/ValueSetRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ValueSetRepository extends JpaRepository<ValueSet, Long> {
     List<ValueSet> findAllByCodedConceptsId(Long id);
+    Optional<ValueSet> findById(Long id);
 
 }
