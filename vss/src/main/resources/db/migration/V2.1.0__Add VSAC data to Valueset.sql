02565f585709635e72481f5fc5df8de52811f718
Sadhana Chandravanka <sadhana.chandravanka@feisystems.com>
2017-10-13 10:12:18 -0400

    Adjustments to consider long name and description along with removal of unique coded concept code
    #15809 - As a developer, I will use the new pix-pdq api for patient registration and patient update

--- vss/src/main/resources/db/migration/V2.1.0__Add VSAC data to Valueset.sql
+++ vss/src/main/resources/db/migration/V2.1.0__Add VSAC data to Valueset.sql
@@ -1,2 +1,9 @@
 Alter TABLE value_set ADD vsac_value_set_name VARCHAR(255), ADD vsac_oid VARCHAR(255);
 Alter TABLE value_set_aud ADD vsac_value_set_name VARCHAR(255), ADD vsac_oid VARCHAR(255);
+
+ALTER TABLE coded_concept
+CHANGE COLUMN `name` `name` MEDIUMTEXT NULL DEFAULT NULL ,
+CHANGE COLUMN `description` `description` MEDIUMTEXT NULL DEFAULT NULL ;
+
+ALTER TABLE coded_concept
+  DROP INDEX `UK_ja446cnbaamiey4p3h1wi5w0r`;
